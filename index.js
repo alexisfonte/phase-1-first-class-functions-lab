c3c5953ce3a74b17f91b79ffbe71eec8ab868c17
Alexis Fonte <alexis.e.fonte@gmail.com>
2022-11-22 02:26:33 -0600

    commit tree f3fcd82ac5731e97fc70366dedf98b43463b148c

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution in this file!
+let drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+const returnFirstTwoDrivers = function() {
+    let newDrivers = drivers.slice(0,2);
+    return newDrivers;
+};
+
+const returnLastTwoDrivers = function() {
+    let driverCount = drivers.length;
+    let lastDrivers = drivers.slice(driverCount - 2, driverCount);
+    return lastDrivers;
+};
+
+let selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(interger) {
+    const fareMultiplier = fare => (fare * interger);
+    return fareMultiplier;
+}
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers(drivers, driverFunction){
+    return driverFunction(drivers);
+}
